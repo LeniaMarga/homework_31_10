0a3205488b0d4a52c42877813fd77270860264ef
LeniaMarga <ekamarga@gmail.com>
2017-10-31 18:28:38 +0000

    Homework_18_28_finished

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -1,3 +1,110 @@
 def return_10
    return 10
 end
+
+def add(number_1, number_2)
+  return number_1.to_i + number_2.to_i
+end
+
+
+def subtract(number_1, number_2)
+  return number_1.to_i - number_2.to_i
+end
+
+
+def multiply(number_1, number_2)
+  return number_1.to_i * number_2.to_i
+end
+
+
+def divide(number_1, number_2)
+ return number_1.to_i / number_2.to_i
+end
+
+
+def length_of_string(str)
+  return str.to_s.length
+end
+
+def join_string(string_1, string_2)
+  return "#{string_1.to_s}#{string_2.to_s}"
+end
+
+def add_string_as_number(string_1, string_2)
+  return string_1.to_i + string_2.to_i
+end
+
+def number_to_full_month_name(number)
+  case number
+    when 12
+    return "December"
+    when 11
+    return  "November"
+    when 10
+    return  "October"
+    when 9
+    return  "September"
+    when 8
+    return  "August"
+    when 7
+    return  "July"
+    when 6
+    return  "June"
+    when 5
+    return  "May"
+    when 4
+    return  "April"
+    when 3
+    return  "March"
+    when 2
+    return  "February"
+    when 1
+    return "January"
+  end
+end
+
+def number_to_short_month_name(number)
+  case number
+    when 12
+    return "Dec"
+    when 11
+    return  "Nov"
+    when 10
+    return  "Oct"
+    when 9
+    return  "Sep"
+    when 8
+    return  "Aug"
+    when 7
+    return  "Jul"
+    when 6
+    return  "Jun"
+    when 5
+    return  "May"
+    when 4
+    return  "Apr"
+    when 3
+    return  "Mar"
+    when 2
+    return  "Feb"
+    when 1
+    return  "Jan"
+  end
+end
+
+
+def side_length_to_cube_volume(number)
+  return number * number * number
+end
+
+def radius_length_to_sphere_volume(number)
+     p_1 = 4/3.to_f
+     p_2 = 3.14
+     volume_maths = (p_1 * p_2) * (number**3)
+     return volume_maths.to_f.round(2)
+end
+
+def fahrenheit_to_celsius(number)
+     celsius_degrees = (number - 32) / 1.8
+     return celsius_degrees.to_f.round(2)
+end
--- specs/ruby_functions_spec.rb
+++ specs/ruby_functions_spec.rb
@@ -7,7 +7,7 @@ class Functions_Practice < MiniTest::Test
     return_10_result = return_10()
     assert_equal( 10, return_10_result )
   end
-=begin
+
   def test_add()
     add_result = add( 1, 2 )
     assert_equal( 3, add_result )
@@ -29,9 +29,9 @@ class Functions_Practice < MiniTest::Test
   end
 
   def test_length_of_string()
-    test_string = "A string of length 21"
-    length_of_string = length_of_string( test_string )
-    assert_equal( 21, length_of_string )
+    # test_string = "A string of length 21"
+    l = length_of_string( "A string of length 21" )
+    assert_equal( 21, l )
   end
 
   def test_join_string()
@@ -70,18 +70,24 @@ class Functions_Practice < MiniTest::Test
 
   #Given the length of a side of a cube calculate the volume
   def test_volume_of_cube()
+    volume_result = side_length_to_cube_volume(3)
+    assert_equal( 27, volume_result)
     #add test code here
   end
 
   #Given the radius of a sphere calculate the volume
   def test_volume_of_sphere()
+    volume_result = radius_length_to_sphere_volume(3)
+    assert_equal( 113.04, volume_result)
     #add test code here
   end
 
   #Given a value in farenheit, convert this into celsius.
   def test_fahrenheit_to_celsius()
+  celsius_degrees = fahrenheit_to_celsius(48)
+  assert_equal( 8.89, celsius_degrees)
     #add test code here
   end
-=end
+
 
 end
